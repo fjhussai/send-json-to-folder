e5b57f14e541b3b0fff75aa6c72d3775b12a2533
fjhussai <farwajhussain@gmail.com>
2019-11-20 11:31:27 -0600

    first commit

---
+++ main.go
@@ -0,0 +1,35 @@
+package main
+
+import (
+	"fmt"
+	"encoding/json"
+	"io/ioutil"
+	"os/exec"
+)
+
+// Potato = sample structure
+type Potato struct {
+	FirstName string `json:"firstname"`
+	LastName string	`json:"lastname"`
+}
+
+//function to export a json
+func export(fname, lname string) {
+	potato := &Potato {
+		FirstName: fname,
+		LastName: lname }
+	potatoByte, _ := json.Marshal(potato)
+
+	err := ioutil.WriteFile("output.json", potatoByte, 0644)
+	if err != nil {
+		fmt.Println(err)
+	}
+
+	exec.Command("mv", "output.json", "./Outputs").Run()
+}
+
+func main() {
+	fmt.Println("Hello World!")
+
+	export("david", "chang")
+}
